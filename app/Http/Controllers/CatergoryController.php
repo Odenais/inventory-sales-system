d696185389c94cc489c8bad9a16768120e1325d2
Odenais <odenaisdev@outlook.com>
2026-02-17 21:44:24 -0600

    feat: base models and controllers for inventory system

---
+++ app/Http/Controllers/CatergoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Catergory;
+use Illuminate\Http\Request;
+
+class CatergoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Catergory $catergory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Catergory $catergory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Catergory $catergory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Catergory $catergory)
+    {
+        //
+    }
+}

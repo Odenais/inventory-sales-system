67563c5b770e3013d0998d0bce97098d0be9e61c
Odenais <odenaisdev@outlook.com>
2026-02-19 20:19:34 -0600

    feat: delete category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -79,6 +79,7 @@ class CategoryController extends Controller
      */
     public function destroy(Category $Category)
     {
-        //
+        $Category->delete();
+        return redirect()->route('categories.index');
     }
 }

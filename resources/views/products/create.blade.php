0fca6bec9d6636d510a5e7ac07b44bba2620afa3
Odenais <odenaisdev@outlook.com>
2026-03-15 18:39:55 -0600

    fix: add method post, add name and id for select categories, and change type to number

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,10 +1,10 @@
-<form action="{{  route('products.store') }}">
+<form action="{{ route('products.store') }}" method="POST">
     @csrf
     <label for="name_product">Name:</label>
     <input type="text" name="name_product" id="name_product" value="">
     <br>
     <label for="category_product">Category:</label>
-    <Select>
+    <select name="category_product_id" id="category_product_id">
         <option value="" selected="selected">- SELECT -</option>
         @foreach ($categories as $category)
         <option value="{{ $category->id }}">{{ $category->name }}</option>
@@ -15,7 +15,7 @@
     <input type="text" name="description_product" id="description_product" value="">
     <br>
     <label for="price_product">Price:</label>
-    <input type="decimal" name="price_product" id="price_product" value="">
+    <input type="number" name="price_product" id="price_product">
     <br>
     <label for="stock_product">Stock:</label>
     <input type="number" name="stock_product" id="stock_product" value="">

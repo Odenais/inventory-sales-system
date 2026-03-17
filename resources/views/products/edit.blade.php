6342ef5c917d3ba8ff3b6bdb7cf079d4d3041f57
Odenais <odenaisdev@outlook.com>
2026-03-16 19:53:43 -0600

    fix: method post form, remove "_products" in name, imagen to image

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,11 +1,11 @@
-<form action="{{  route('products.update', $product) }}">
+<form action="{{  route('products.update', $product) }}" method="POST" enctype="multipart/form-data">
     @csrf
     @method('PUT')
-    <label for="name_product">Name:</label>
-    <input type="text" name="name_product" id="name_product" value="{{  $product->name }}">
+    <label for="name">Name:</label>
+    <input autocomplete="off" type="text" name="name" id="name" value="{{  $product->name }}">
     <br>
-    <label for="category_product">Category:</label>
-    <Select>
+    <label for="category_id">Category:</label>
+    <Select name="category_id" id="category_id">
         @foreach ($categories as $category)
         @if ($category->id == $product->category_id)
         <option value="{{ $category->id }}" selected="selected">{{ $category->name }}</option>
@@ -14,18 +14,18 @@
         @endforeach
     </Select>
     <br>
-    <label for="description_product">Description:</label>
-    <input type="text" name="description_product" id="description_product" value="{{ $product->description }}">
+    <label for="description">Description:</label>
+    <input autocomplete="off" type="text" name="description" id="description" value="{{ $product->description }}">
     <br>
-    <label for="price_product">Price:</label>
-    <input type="decimal" name="price_product" id="price_product" value="{{ $product->price }}">
+    <label for="price">Price:</label>
+    <input autocomplete="off" type="decimal" name="price" id="price" value="{{ $product->price }}">
     <br>
-    <label for="stock_product">Stock:</label>
-    <input type="number" name="stock_product" id="stock_product" value="{{ $product->stock }}">
+    <label for="stock">Stock:</label>
+    <input autocomplete="off" type="number" name="stock" id="stock" value="{{ $product->stock }}">
     <br>
-    <label for="imagen_product"></label>
+    <label for="image"></label>
     <img src="{{  asset('storage/' . $product->image) }}" alt="" width="150px">
     <br>
-    <input type="file" name="imagen_product" id="imagen_product">
+    <input type="file" name="image" id="image">
     <input type="submit" value="Update">
 </form>
